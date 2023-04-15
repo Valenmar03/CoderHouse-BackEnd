58f480f6da79abd16e500c0334e35508bf10d835
Valenmar03 <valenmrtnz8@gmail.com>
2023-04-15 14:40:41 -0300

    Elimino metodos que no sirven del index.js

--- index.js
+++ index.js
@@ -14,16 +14,9 @@ const context = async () => {
 
   // await productManager.addProducts(testProduct)
 
-  //await productManager.getProducts();
-
-  //const id1 = await productManager.getProductById(2)
-  //console.log(id1)
-
   //await productManager.deleteProduct(6, 5, 4, 3);
 
   // await productManager.updateProduct(2, 'Ford F-150', 'Ford F-150 Raptor',  20000, 'imagen.jpg', '124h143g32', 1)
-
-  //console.log(await productManager.getProducts())
 
 };
 
