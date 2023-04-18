4e1bb5751595d77d72eb95b59e3a0ff90b40f7d3
Valenmar03 <valenmrtnz8@gmail.com>
2023-04-18 10:02:40 -0300

    Status error en sends de products

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -11,9 +11,12 @@ router.get("/", async (req, res) => {
   const limit = req.query.limit;
   const parseLimit = parseInt(limit);
   if (!limit || parseLimit > products.length) {
-    res.send(products)
+    res.send({status: 'success', payload: products})
+    res.status(400).send({status: 'error', error: 'Ocurrio un error'});
   } else {
-    res.send(products.slice(0, parseLimit));
+    const productsLimit = products.slice(0, parseLimit)
+    res.send({status: 'success', payload: productsLimit});
+    res.status(400).send({status: 'error', error: 'Ocurrio un error'})
   }
 });
 
@@ -21,7 +24,7 @@ router.get("/:pid", async (req, res) => {
   const paramId = Object.values(req.params)[0];
   const id = parseInt(paramId);
   const product = await productManager.getProductById(id);
-  res.send(product);
+  res.send({status: 'success', payload: product});
 });
 
 router.post('/', async (req, res) => {
