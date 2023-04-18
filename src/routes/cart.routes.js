3f98ee78f6bdf4e3368afbd97d256df9be57cfd7
Valenmar03 <valenmrtnz8@gmail.com>
2023-04-18 15:45:23 -0300

    modificacion metodo para que devulva solo los prods

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -16,7 +16,7 @@ router.get('/:cid', async (req, res) => {
     const paramId = Object.values(req.params)[0]
     const id = parseInt(paramId)
     const cart = await cartManager.getCartById(id)
-    res.send({status: 'success', payload: cart})
+    res.send({status: 'success', payload: cart.products})
 })
 
 router.post('/:cid/products/:pid', async (req, res) => {
