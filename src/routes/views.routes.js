9ffd949a8a7d927dcc7ce52f61477bfb721dd532
Valenmar03 <valenmrtnz8@gmail.com>
2023-06-03 19:29:48 -0300

    Views de detalles de producto, sin addPrToCart

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -15,9 +15,6 @@ router.get("/", async (req, res) => {
   const { page = 1 } = req.query;
   const sort = req.query.sort;
   const category = req.query.category;
-
-  console.log(sort);
-  console.log(category);
   const { docs, hasPrevPage, hasNextPage, prevPage, nextPage, ...rest } =
     await productService.getProducts(page, sort, category);
 
@@ -46,9 +43,18 @@ router.get("/chat", async (req, res) => {
   res.render("chat");
 });
 
+router.get("/products/:pid", async (req, res) => {
+  const { pid } = req.params;
+  const product = await productService.getProductById({ _id: pid });
+  res.render('prodDetails', {
+    css: 'product',
+    prod: product
+  })
+});
+
 router.get("/carts/:cid", async (req, res) => {
-  const { cid} = req.params;
-  const cart = await cartService.getCartById({ _id: cid })
+  const { cid } = req.params;
+  const cart = await cartService.getCartById({ _id: cid });
   res.render("carts", {
     css: "cart",
     prod: cart.products,
